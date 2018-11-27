ea9c7ec447974ecd071800e7fa383b9ba3f5becf
JohannesSpicker <johannesspicker@me.com>
2018-11-27 10:31:36 +0100

    StringCollection added

---
+++ SilentPac_0.02/Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public bool useController = true;
+    
+    void FixedUpdate()
+    {
+
+        if (useController)
+        {
+            //Left Joystick: Move control
+            float moveHorizontal = Input.GetAxis(StringCollection.INPUT_HORIZONTAL);
+            float moveVertical = Input.GetAxis(StringCollection.INPUT_VERTICAL);
+
+            //Right Joystick: Rotation control
+            float rotateHorizontal = Input.GetAxis(StringCollection.INPUT_RHORIZONTAL);
+            float rotateVertical = Input.GetAxis(StringCollection.INPUT_RVERTICAL);
+
+            /* //rotate player to RStickposition
+            Vector3 playerDirection = Vector3.right * Input.GetAxisRaw(StringCollection.INPUT_RHORIZONTAL) + Vector3.forward * -Input.GetAxisRaw(StringCollection.INPUT_RVERTICAL);
+            if (playerDirection.sqrMagnitude > 0.0f)
+            {
+                transform.rotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+
+            }
+            */
+
+            //Buttons A/B/X/Y
+
+            if (Input.GetButtonDown(StringCollection.INPUT_A))
+            {
+                //use glove*
+                Debug.Log("A");
+            }
+
+            if (Input.GetButtonDown(StringCollection.INPUT_B))
+            {
+                //shoot charge
+                Debug.Log("B");
+
+            }
+
+            if (Input.GetButtonDown(StringCollection.INPUT_X))
+            {
+                //drop charge
+                Debug.Log("X");
+
+            }
+
+            if (Input.GetButtonDown(StringCollection.INPUT_Y))
+            {
+                //not used
+                Debug.Log("Y");
+
+            }
+
+            //Buttons LB/RB
+
+            if (Input.GetButtonDown(StringCollection.INPUT_LB))
+            {
+                //not used
+                Debug.Log("LB");
+
+            }
+
+            if (Input.GetButtonDown(StringCollection.INPUT_RB))
+            {
+                //not used
+                Debug.Log("RB");
+
+            }
+
+            //Buttons LT/RT
+
+            if (Input.GetAxis(StringCollection.INPUT_LT) != 0)
+            {
+                //run
+                Debug.Log("LT");
+
+            }
+
+            if (Input.GetAxis(StringCollection.INPUT_RT) != 0)
+            {
+                //activate glove
+                Debug.Log("RT");
+
+            }
+
+            //Buttons Select/Start
+
+            if (Input.GetButtonDown(StringCollection.INPUT_SELECT))
+            {
+                //not used
+                Debug.Log("Select");
+
+            }
+
+            if (Input.GetButtonDown(StringCollection.INPUT_START))
+            {
+                //not used
+                Debug.Log("Start");
+
+            }
+
+            //Buttons Stickpress
+
+            if (Input.GetButtonDown(StringCollection.INPUT_LSTICKPRESS))
+            {
+                //not used
+                Debug.Log("LStickpress");
+
+            }
+
+            if (Input.GetButtonDown(StringCollection.INPUT_RSTICKPRESS))
+            {
+                //not used
+                Debug.Log("RStickpress");
+
+            }
+
+
+        }
+    }
+}
---
+++ SilentPac_0.02/Assets/Scripts/Unapplied/StringCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringCollection
+{
+
+    //X-Box Controller (360 & One)
+    public const string INPUT_HORIZONTAL = "Horizontal";    //Axis x
+    public const string INPUT_VERTICAL = "Vertical";        //Axis y
+    public const string INPUT_RHORIZONTAL = "RHorizontal";  //Axis 4
+    public const string INPUT_RVERTICAL = "RVertical";      //Axis 5
+    public const string INPUT_DHORIZONTAL = "DHorizontal";  //Axis 6
+    public const string INPUT_DVERTICAL = "DVertical";      //Axis 7
+    public const string INPUT_LT = "LT";                    //Axis  9
+    public const string INPUT_RT = "RT";                    //Axis 10
+
+    public const string INPUT_A = "Fire1";                  //Button 0
+    public const string INPUT_B = "Fire2";                  //Button 1
+    public const string INPUT_X = "Fire3";                  //Button 2
+    public const string INPUT_Y = "Fire4";                  //Button 3
+    public const string INPUT_LB = "LB";                    //Button 4
+    public const string INPUT_RB = "RB";                    //Button 5
+    public const string INPUT_SELECT = "Select";            //Button 6
+    public const string INPUT_START = "Start";              //Button 7
+    public const string INPUT_LSTICKPRESS = "LStickpress";  //Button 8
+    public const string INPUT_RSTICKPRESS = "RStickpress";  //Button 9
+}
