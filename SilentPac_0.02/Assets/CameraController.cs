ded1976d347c1d467544fe6e2ca04deee555386e
SchiefesSchiff <Rene.Gnahs@gmx.de>
2018-12-06 01:44:43 +0100

    Action Mode implemented

--- SilentPac_0.02/Assets/CameraController.cs
+++ SilentPac_0.02/Assets/CameraController.cs
@@ -8,7 +8,12 @@ public class CameraController : MonoBehaviour
     public Vector3 offsetPos;
     public float SmoothnessCameraMove = 5f;
     public float turnSpeed = 10f;
+    public float smoothSpeed = 5f;
+    public float rotationSpeed = 10;
+    public GameObject SecondCameraPos;
 
+    public bool Alarm = false;
+    private bool lastInput = true;
     private Quaternion targetRotation;
     private Vector3 targetPos;
     public float angle;
@@ -22,13 +27,28 @@ public class CameraController : MonoBehaviour
     {
         InputCameraControll();
 
-        RotateARoundTarget(rotationX);
-        MoveWithTarget();
-        LookAtTarget();
-        RotateBehindPlayer();
+        if (!Alarm)
+        {
+            RotateARoundTarget(rotationX);
+            MoveWithTarget();
+            LookAtTarget();
+            RotateBehindPlayer();
+        }
+        else
+        {
+            MoveToSecondPos();
+        }
 
     }
 
+    void MoveToSecondPos()
+    {
+        if (transform.position != SecondCameraPos.transform.position)
+        {
+           transform.position = Vector3.Lerp(transform.position, SecondCameraPos.transform.position, 1.5f * Time.deltaTime);
+           transform.rotation = Quaternion.Slerp(transform.rotation, SecondCameraPos.transform.rotation, 1 * Time.deltaTime);
+        }
+    }
 
     void InputCameraControll()
     {
@@ -58,28 +78,24 @@ public class CameraController : MonoBehaviour
     void RotateARoundTarget(float angle)
     {
         Vector3 vel = Vector3.zero;
-        Vector3 targetOffsetPos = Quaternion.Euler(0, angle, 0) * offsetPos;
-        //float dist = Vector3.Distance(offsetPos, targetOffsetPos);
+        Vector3 targetOffsetPos = Quaternion.Euler(0, angle * rotationSpeed, 0) * offsetPos;
+        float dist = Vector3.Distance(offsetPos, targetOffsetPos);
 
-        //while (dist > 0.02f)
-        //{
-        //    offsetPos = Vector3.SmoothDamp(offsetPos, targetOffsetPos, ref vel, smoothSpeed);
-        //    dist = Vector3.Distance(offsetPos, targetOffsetPos);
-        //}
+        while (dist > 0.02f)
+        {
+            offsetPos = Vector3.SmoothDamp(offsetPos, targetOffsetPos, ref vel, smoothSpeed);
+            dist = Vector3.Distance(offsetPos, targetOffsetPos);
+        }
 
         offsetPos = targetOffsetPos;
 
     }
 
-        bool lastInput = true;
 
     private bool isInputRighJoy()
     {
-
-
-        if (rotationX != 0 || rotationY !=0)
+        if (rotationX != 0 || rotationY != 0)
         {
-            lastInput = false;
             return false;
         }
 
@@ -88,13 +104,26 @@ public class CameraController : MonoBehaviour
             lastInput = true;
             return true;
         }
-
-        if (lastInput == true)
-        {
-            return true;
-        }
-
         return false;
+
+        //if (rotationX != 0 || rotationY !=0)
+        //{
+        //    lastInput = false;
+        //    return false;
+        //}
+
+        //if (input.y != 0 || input.y != 0)
+        //{
+        //    lastInput = true;
+        //    return true;
+        //}
+
+        //if (lastInput == true)
+        //{
+        //    return true;
+        //}
+
+        //return false;
 
     }
 
