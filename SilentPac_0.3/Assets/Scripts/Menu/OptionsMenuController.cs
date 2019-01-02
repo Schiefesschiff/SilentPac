a46ffce047098822ea23de1fb8cc5c9d9a3ae4ea
JohannesSpicker <johannesspicker@me.com>
2019-01-02 18:09:48 +0100

    MainMenu added
    - sliders not implemented yet

---
+++ SilentPac_0.3/Assets/Scripts/Menu/OptionsMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenuController : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetBrightness(float newBrightness)
+    {
+
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log("Setting Volume to " + volume + ".");
+        audioMixer.SetFloat("volume", volume);
+    }
+
+}
